18e90b2124918970e68a163d17ad41873309260e
Jero Alvarez <jeroalva@gmail.com>
2020-02-04 18:44:30 -0600

    Updates

---
+++ precache-manifest.d61262bc324440ddc27a086d4b5ef7ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffdaa7f76c0584b482021485b262208a",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "14c8c609329a00643a43",
+    "url": "/clickygame/static/css/main.a4a9b32f.chunk.css"
+  },
+  {
+    "revision": "f5c0e04957ce5462e24d",
+    "url": "/clickygame/static/js/2.d483b16d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/clickygame/static/js/2.d483b16d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "14c8c609329a00643a43",
+    "url": "/clickygame/static/js/main.d1853d62.chunk.js"
+  },
+  {
+    "revision": "b09beb9ce352a339b592",
+    "url": "/clickygame/static/js/runtime-main.e3b59e45.js"
+  }
+]);
