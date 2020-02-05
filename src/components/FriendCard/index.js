69d6d54554895cd34e8af5f427a15139e291d917
Jero Alvarez <jeroalva@gmail.com>
2020-02-04 19:08:55 -0600

    Agregue un espacio para poder hacer add y commit

--- src/components/FriendCard/index.js
+++ src/components/FriendCard/index.js
@@ -4,7 +4,7 @@ import "./style.css";
 function FriendCard(props) {
   return (
     <div className="btn btn-default" onClick={props.clickFriend}>
-      <img src={props.image} width="150" id={props.id} data-name={props.name} alt={props.name}/>
+      <img src={props.image} width="150"  id={props.id} data-name={props.name} alt={props.name}/>
     </div>
   );
 }
